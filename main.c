c1201b9d0b9f4ca977caab95a9a19a0d316c233b
RenzoRomeo <renzoromeo44@gmail.com>
2024-04-17 21:53:52 -0300

    error si directorio destino no está vacío

--- main.c
+++ main.c
@@ -1,18 +1,29 @@
 #include "daemonize.h"
 #include "monitor.h"
 
+#include <errno.h>
 #include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <syslog.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  // TODO: permitir iniciar y detener el daemon.
   // TODO: el daemon no debe poder estar instanciado más de una vez con el mismo
   // directorio de destino para cualquier momento dado.
+  //
+  // 1. el destination path identifica unívocamente a cada instancia del daemon.
+  // 2. almacenar en un archivo (pid_file) un mapa {destination_path: pid}
+  // 3. al ejecutar daemon stop <destination_path>, parsear archivo y hacer
+  // kill(pid) de la instancia que corresponda.
+  //
   // TODO: permitir directorios
+  //
+  // TODO: usos:
+  //  daemon start <source_dir> <destination_dir> (argc == 4)
+  //  daemon stop <destination_dir> (argc == 4)
 
   if (argc != 3) {
     printf("Uso: daemon <directorio a supervisar> <directorio destino>\n");
@@ -26,12 +37,20 @@ int main(int argc, char *argv[]) {
 
   char destination_path[PATH_MAX];
   realpath(argv[2], destination_path);
+
+  int result = rmdir(destination_path);
+  if (result != 0 && errno == ENOTEMPTY) {
+    syslog(LOG_ERR, "Destination directory is not empty: %s\n",
+           destination_path);
+    printf("Destination directory is not empty: %s\n", destination_path);
+    exit(EXIT_FAILURE);
+  }
+
   struct stat st = {0};
   if (stat(destination_path, &st) == -1) {
     mkdir(destination_path, 0700);
     syslog(LOG_NOTICE, "Created destination directory: %s\n", destination_path);
   }
-  // TODO: si el directorio no está vacío, salir con error.
 
   // TODO: pensar un nombre bueno
   const char *name = "File Enctryptor";
