267b1e5a2df4de3d9a625af707eb152206a71250
RenzoRomeo <renzoromeo44@gmail.com>
2024-04-20 19:32:58 -0300

    nombre de daemon como variable de script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+DAEMON_NAME="daemon"
 CFLAGS="-Wall -Wextra -g"
 
-gcc main.c monitor.c daemonize.c -o daemon $CFLAGS
+gcc main.c monitor.c daemonize.c -o $DAEMON_NAME $CFLAGS
 gcc decrypt.c -o decrypt $CFLAGS
