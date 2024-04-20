a363fd4a532466509cea5119915bdc1ab428e8f8
RenzoRomeo <renzoromeo44@gmail.com>
2024-04-20 19:51:56 -0300

    directorio desenctriptado se crea al mismo nivel que el directorio encriptado

--- decrypt.c
+++ decrypt.c
@@ -1,15 +1,17 @@
 #include <dirent.h>
 #include <errno.h>
+#include <libgen.h>
 #include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define ERROR(FMT, ...)                                                        \
   fprintf(stderr, FMT ": %s\n", ##__VA_ARGS__, strerror(errno))
 
-void encryptDecryptFile(const char *input_file_name, const char *output_file_name,
-                        char *key) {
+void decrypt_file(const char *input_file_name,
+                        const char *output_file_name, char *key) {
   FILE *input_file, *output_file;
   char ch;
   int key_index = 0;
@@ -49,15 +51,29 @@ void decrypt_directory(const char *directory_path, char *key) {
     exit(EXIT_FAILURE);
   }
 
+  char parent_dir[PATH_MAX];
+  strncpy(parent_dir, directory_path, PATH_MAX);
+  char *parent = dirname(parent_dir);
+
+  char decrypted_dir[PATH_MAX];
+  snprintf(decrypted_dir, PATH_MAX, "%s/decrypted_%s", parent,
+           basename((char *)directory_path));
+  if (mkdir(decrypted_dir, 0777) == -1) {
+    if (errno != EEXIST) {
+      ERROR("Error creating directory '%s'", decrypted_dir);
+      exit(EXIT_FAILURE);
+    }
+  }
+
   while ((entry = readdir(dir)) != NULL) {
     if (entry->d_type == DT_REG) {
       char input_file[PATH_MAX];
       char output_file[PATH_MAX];
 
-      sprintf(input_file, "%s/%s", directory_path, entry->d_name);
-      sprintf(output_file, "%s/decrypted_%s", directory_path, entry->d_name);
+      snprintf(input_file, PATH_MAX, "%s/%s", directory_path, entry->d_name);
+      snprintf(output_file, PATH_MAX, "%s/%s", decrypted_dir, entry->d_name);
 
-      encryptDecryptFile(input_file, output_file, key);
+      decrypt_file(input_file, output_file, key);
     }
   }
 
@@ -65,6 +81,8 @@ void decrypt_directory(const char *directory_path, char *key) {
 }
 
 int main(int argc, char *argv[]) {
+  // TODO: arreglar warnings del compilador
+  
   if (argc != 3) {
     printf("Usage: %s <directory_path> <encryption_key>\n", argv[0]);
     return 1;
